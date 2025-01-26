e0bb2ad0db53f01fd6a563879023999b9d046344
Fikriansyah000 <fikriansyahhr@upi.edu>
2025-01-26 19:12:51 +0700

    Memodifikasi beberapa file di php

--- datajson/latihan1.php
+++ datajson/latihan1.php
@@ -46,9 +46,9 @@ $menu = $menu["menu"];
           <?php foreach($menu as $row ): ?>
                 <div class="col-md-4">
                 <div class="card text-white bg-warning mb-3 " >
-                    <img class="card-img-top" src="../img/BottleCreamyVanilla.png" alt="Card image cap">
+                    <img class="card-img-top" src="../<?= $row["img"]; ?>" alt="Card image cap">
                     <div class="card-body">
-                        <h5 class="card-title">Kopi Ayam Manis </h5>
+                        <h5 class="card-title"><?= $row["nama"]; ?> </h5>
                         <h6 class="card-subtitle mb-2 text-muted">Rp. 50.000</h6>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         <a href="#" class="btn btn-danger">Checkout This!</a>
